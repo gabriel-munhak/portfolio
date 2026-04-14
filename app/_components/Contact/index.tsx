830ab85b9a9b2b425dad3b862f22d25d678dec30
Heclypso <heclypso@gmail.com>
2026-04-14 13:11:40 -0300

    #11 feat: desenvolvendo o componente Contact.

--- app/_components/Contact/index.tsx
+++ app/_components/Contact/index.tsx
@@ -3,8 +3,8 @@ import ContactIconsList from "../ContactIconsList"
 const Contact = () => {
     return (
         <div className="container flex flex-col items-center justify-center">
-            <h2 className="text-[30px] font-black mb-[32px] text-[var(--secondaryColor)]">Entre em contato</h2>
-            <p className="w-[480px] text-center mb-[30px]">
+            <h2 className="lg:text-[1.875rem] md:text-[1.25rem] font-black lg:mb-[2rem] md:mb-[1rem] text-[var(--secondaryColor)]">Entre em contato</h2>
+            <p className="w-[30rem] lg:text-[1rem] md:text-[0.875rem] text-center lg:mb-[1.875rem] md:mb-[1rem]">
                 Conheça mais sobre mim ao explorar meu portfólio. Aqui você encontra um resumo pessoal, minhas experiências acadêmicas e profissionais, além dos projetos e atividades mais recentes.
             </p>
             <ContactIconsList />
